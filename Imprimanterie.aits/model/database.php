25321ab19648a1492074c9a8d3c8e7d967a28649
hayslipco <wqe>
2019-12-17 08:25:32 +0100

    Nettoyage de database

--- Imprimanterie.aits/model/database.php
+++ Imprimanterie.aits/model/database.php
@@ -15,20 +15,6 @@ class database{
         }
     }
 
-    /*
-    
-    __construct() : créer un nouvel objet PDO et se connecter à la BD
-    queryExecute($query) : permet de préparer et d’exécuter une requête de type simple (sans where)
-    prepareExecute($query) : permet de préparer, de binder et d’exécuter une requête (select avec where ou insert, update et delete)
-    fetchData($mode) : traiter les données pour les retourner par exemple en tableau associatif (avec PDO::FETCH_ASSOC)
-    closeCursor() : vide le jeu d’enregistrement
-    __destruct() : supprimer l’objet
-    getAllTeachers() : récupère la liste de tous les enseignants de la BD
-    getOneTeachers($id) : récupère la liste des informations pour 1 enseignant
-    ... + tous les autres méthodes dont vous aurez besoin pour la suite (insertTeacher ... etc)
-
-    */
-
     function queryExecute($query){
         $this->closeCursor();
         $this->req = $this->connector->query($query);
@@ -61,25 +47,6 @@ class database{
     function __destruct()
     {
         $this->connector = null;
-    }
-
-    function getAllTeachers(){
-        $this->queryExecute('SELECT t.teaName AS ln, t.teaFirstname AS fn, n.nickName AS nn FROM t_teacher t, t_nickname n WHERE t.idTeacher = n.idTeacher  ');
-        return $this->fetchData(PDO::FETCH_ASSOC);
-    }
-
-    function getOneTecher($id){
-        $query = "SELECT * FROM t_teacher WHERE idTeacher = :id";
-        $params = array(array("name"=> "id", "value"=> $id, "type"=> PDO::PARAM_INT));
-
-        $this->prepareExecute($query, $params);
-        $this->fetchData(PDO::FETCH_ASSOC);
-
-    }
-
-    function getAllTeachNames(){
-        $this->queryExecute('SELECT t.teaName AS ln, t.teaFirstname AS fn, n.nickName AS nn FROM t_teacher t, t_nickname n WHERE t.idTeacher = n.idTeacher  ');
-        return $this->fetchData(PDO::FETCH_ASSOC);
     }
 
     function fetchTable(){
